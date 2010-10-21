493ef26bd439955afc14246a83570693f07a5fd1
straup <straup@gmail.com>
2010-10-21 23:13:10 +0000

    urls for indivisdual buckets and rows

--- www/include/lib_urls.php
+++ www/include/lib_urls.php
@@ -13,17 +13,34 @@
 
 	#################################################################
 
+	function urls_url_for_bucket(&$bucket){
+
+		$user = users_get_by_id($bucket['user_id']);
+		return urls_buckets_for_user($user) . "{$bucket['id']}/";
+	}
+
+	#################################################################
+
+	function urls_url_for_dot(&$dot){
+
+		$user = users_get_by_id($dot['user_id']);
+		return urls_buckets_for_user($user) . "{$dot['id']}/";
+	}
+
+	#################################################################
+
 	function urls_dots_for_user(&$user){
 
-		return urls_url_for_user($user) . "dots";
+		return urls_url_for_user($user) . "dots/";
 	}
 
 	#################################################################
 
 	function urls_buckets_for_user(&$user){
 
-		return urls_url_for_user($user) . "buckets";
+		return urls_url_for_user($user) . "buckets/";
 	}
 
 	#################################################################
+
 ?>
