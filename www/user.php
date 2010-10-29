0761233bb54ad98065858e046b09a2b8d8cd2439
straup <straup@gmail.com>
2010-10-29 02:18:40 +0000

    maps on the buckets page and dashboard

--- www/user.php
+++ www/user.php
@@ -18,6 +18,22 @@
 
 	$owner['counts'] = buckets_counts_for_user($owner, $GLOBALS['cfg']['user']['id']);
 
+	# fetch some recent buckets for this user
+
+	$args = array(
+		'page' => 1,
+		'per_page' => 10,
+	);
+
+	$buckets = buckets_buckets_for_user($owner, $GLOBALS['cfg']['user']['id'], $args);
+
+	$is_own = ($owner['id'] == $GLOBALS['cfg']['user']['id']) ? 1 : 0;
+
+	$smarty->assign("is_own", $is_own);
+	$smarty->assign_by_ref('buckets', $buckets);
+
+	#
+
 	$smarty->display('page_user.txt');
 	exit();
 ?>
