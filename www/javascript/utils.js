e79cc9d8103162d08c306d31ffac88c12d37d52d
straup <straup@gmail.com>
2010-11-02 17:42:42 +0000

    added utils_(poly|modest)map JS functions, updated accordingly

--- www/javascript/utils.js
+++ www/javascript/utils.js
@@ -1,6 +1,54 @@
 // This is probably (hopefully?) just a temporary place-holder for
 // shared/common functions (20101101/straup) 
 
+function utils_polymap(map_id){
+
+	var po = org.polymaps;
+	var svg = po.svg("svg");
+
+	var div = document.getElementById(map_id);
+	div.appendChild(svg);
+
+	var map = po.map();
+	map.container(svg);
+
+	var wheel = po.wheel();
+	wheel.smooth(false);
+	map.add(wheel);
+
+	var drag = po.drag();
+	map.add(drag);
+
+	var dblclick = po.dblclick();	
+	map.add(dblclick);
+
+	var hash = po.hash();
+	map.add(hash);
+
+	var url = po.url("http://{S}tile.cloudmade.com/1a1b06b230af4efdbb989ea99e9841af/998/256/{Z}/{X}/{Y}.png");
+	url.hosts(["a.", "b.", "c.", ""]);
+
+	var tileset = po.image();
+	tileset.url(url);
+
+	map.add(tileset);
+	return map;
+}
+
+function utils_modestmap(map_id){
+
+	var mm = com.modestmaps;
+	
+	var template = 'http://a.tile.cloudmade.com/1a1b06b230af4efdbb989ea99e9841af/998/256/{Z}/{X}/{Y}.png';
+	var provider = new mm.TemplatedMapProvider(template);
+
+	var dims = undefined;
+	var handlers = undefined;
+
+	var map = new mm.Map(map_id, provider, dims, handlers);
+	return map;
+}
+
 // quick and dirty function to tweak the extents of a bounding
 // box so that dots don't get cropped by the edge of the map.
 // this will undoubtedly require finesse-ing over time...
