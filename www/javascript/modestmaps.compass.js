41255594e184c7716355476b88b24c1af1d64141
straup <straup@gmail.com>
2010-12-03 01:04:54 +0000

    make modestmaps.compass work with current raphael

--- www/javascript/modestmaps.compass.js
+++ www/javascript/modestmaps.compass.js
@@ -1,11 +1,5 @@
 // This is Carden's original MapControls thingy tweaked just enough to look
-// like the Polymaps compass thingy. Something about this code does not work
-// with current versions of Raphael. Specifically:
-// "pathArray[0] is undefined  [Break On This Error] if (pathArray[0][0] == "M") { raphael.js ln 648"
-// So we're using an older version (0.7.3) at least until we decide how/what
-// we're using for a JS mapping layer rather then going down the rabbit hole
-// of debugging third-party JS libraries.
-// (20101130/straup)
+// like the Polymaps compass thingy.
 
 // namespacing!
 
@@ -31,13 +25,25 @@ com.modestmaps.Compass = function(map){
 
     // zoom in (background and "+" symbol)
 
-    var zin = this.canvas.path({fill: "#ccc", stroke: "white", 'stroke-width': 3}, "M-12,0V-12A12,12 0 1,1 12,-12V0Z").translate(25, 36);
-    var zina = this.canvas.path({stroke: "white", 'stroke-width': 2}, "M -5 0 L 5 0 M 0 -5 L 0 5").translate(25, 25);
+    var zin = this.canvas.path("M-12,0V-12A12,12 0 1,1 12,-12V0Z").translate(25, 36);
+    zin.attr("fill", "#ccc");
+    zin.attr("stroke", "#fff");
+    zin.attr("stroke-width", 3);
+	
+    var zina = this.canvas.path("M -5 0 L 5 0 M 0 -5 L 0 5").translate(25, 25);
+    zina.attr("stroke", "#fff");
+    zina.attr("stroke-width", 2);
 
     // zoom out (background and "-" symbol)
 
-    var zout = this.canvas.path({fill: "#ccc", stroke: "white", 'stroke-width': 3}, "M-12,0V-12A12,12 0 1,1 12,-12V0Z").translate(25, 65).rotate(180);
-    var zouta = this.canvas.path({stroke: "white", 'stroke-width': 2}, "M -5 0 L 5 0").translate(25, 52);
+    var zout = this.canvas.path("M-12,0V-12A12,12 0 1,1 12,-12V0Z").translate(25, 65).rotate(180);
+    zout.attr("fill", "#ccc");
+    zout.attr("stroke", "#fff");
+    zout.attr("stroke-width", 3);
+
+    var zouta = this.canvas.path("M -5 0 L 5 0").translate(25, 52);
+    zouta.attr("stroke", "#fff");
+    zouta.attr("stroke-width", 2);
 
     zina.node.onclick = zin.node.onclick = function() { map.zoomIn() };
     zouta.node.onclick = zout.node.onclick = function() { map.zoomOut() };
