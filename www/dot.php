31a1398e7390df6115cb10c75fa50e4b2427f154
sconnelley <slconnelley@aim.comslconnelley@aim.com>
2011-02-03 12:51:41 -0800

    simplified pagination a little check for all magical words in dot.php cache all dots query in dot.php

--- www/dot.php
+++ www/dot.php
@@ -18,21 +18,43 @@
 
 	$dot = dots_get_dot($dot_id, $GLOBALS['cfg']['user']['id']);
 	
-	//	quickly determine if dot is magical
-	//	TODO: add checks for other magical types
+	//	quickly determine if dot is a magical one
 	$smarty->assign("dot_is_flickr", ((isset($dot['details']['flickr:id']) && !empty($dot['details']['flickr:id']) ) ? true : false) );
+	$smarty->assign("dot_is_woeid", ((isset($dot['details']['yahoo:woeid']) && !empty($dot['details']['yahoo:woeid']) ) ? true : false) );
+	$smarty->assign("dot_is_oam", ((isset($dot['details']['oam:mapid']) && !empty($dot['details']['oam:mapid']) ) ? true : false) );
+	$smarty->assign("dot_is_walkingpapers", ((isset($dot['details']['walkingpapers:scanid']) && !empty($dot['details']['walkingpapers:scanid']) ) ? true : false) );
+	$smarty->assign("dot_is_foursquare", ((isset($dot['details']['foursquare:venue']) && !empty($dot['details']['foursquare:venue']) ) ? true : false) );
 	
 	//	Setup for dot pagination
-	//	Probably a better way to do pagination on single dot page
-	//	Maybe by including next/previous dot id's in return of dots_get_dot function
+	//	Probably a better way to do pagination on single dot pages
+	//	Possibly by including next/previous dot id's in return of dots_get_dot function
 	if( isset($dot['sheet_id']) ){
 		$sheet = sheets_get_sheet($dot['sheet_id']);
 		if( isset($sheet) && !empty($sheet) ){
 			$more = array(
 				'per_page' => $GLOBALS['cfg']['import_max_records'],
 			);
-			$dots = dots_get_dots_for_sheet($sheet, $GLOBALS['cfg']['user']['id'],$more);
-			if(isset($dots) && !empty($dots)){			
+			
+			//............................. CACHE DOTS ? .......................................//
+			//..................................................................................//
+			$cache_key = "dot_page_dots_{$dot_id}";
+			$cache = cache_get($cache_key);
+			if($cache['ok']){ 
+				$dots = $cache['data'];
+			}else{
+				$dots = dots_get_dots_for_sheet($sheet, $GLOBALS['cfg']['user']['id'],$more);
+			}
+			//....................................................................................//
+			//....................................................................................//
+			
+			if(isset($dots) && !empty($dots)){
+				
+				// set cache
+				cache_set($cache_key, array(
+					'ok' => 1,
+					'data' => $dots,
+				));
+							
 				$dots_total = count($dots) - 1;
 				$dot_spot_in_array = -1;
 				$ct = 0;
@@ -45,11 +67,8 @@
 				}
 
 				if($dot_spot_in_array > -1){
-					$next_dot = $dot_spot_in_array + 1;
-					$prev_dot = $dot_spot_in_array - 1;
-					$smarty->assign_by_ref("dots", $dots);
-					$smarty->assign_by_ref("dot_spot_in_array", $dot_spot_in_array);
-					$smarty->assign_by_ref("dots_total", $dots_total);
+					$next_dot = (isset($dots[$dot_spot_in_array + 1]['id'])) ? $dots[$dot_spot_in_array + 1]['id'] : -1;
+					$prev_dot = (isset($dots[$dot_spot_in_array - 1]['id'])) ? $dots[$dot_spot_in_array - 1]['id'] : -1; 
 					$smarty->assign_by_ref("next_dot", $next_dot);
 					$smarty->assign_by_ref("prev_dot", $prev_dot);
 				}
