5fea58136000a940611f2d912b9ed5d0a945abce
straup <straup@gmail.com>
2011-03-03 21:25:34 +0000

    enable caching for all formats (from a sheet); move some configs in to default config.php

--- www/sheet_export.php
+++ www/sheet_export.php
@@ -74,29 +74,26 @@
 		'viewer_id' => $GLOBALS['cfg']['user']['id'],
 	);
 
-	$valid_extras = array(
-		'height' => null,
-		'width' => null,
-		'dot_size' => null,
-	);
+	# check for user-defined tweaks to the output
+ 
+	if (isset($GLOBALS['cfg']['export_valid_extras'][$format])){
 
-	foreach ($valid_extras as $extra => $details){
+		$valid_extras = $GLOBALS['cfg']['export_valid_extras'][$format];
 
-		$what = get_str($extra);
+		foreach ($valid_extras as $extra => $details){
 
-		if (! $what){
-			continue;
+			$what = get_str($extra);
+
+			if (! $what){
+				continue;
+			}
+
+			if ((is_array($details)) && (! in_array($what, $details))){
+				continue;
+			}
+
+			$export_more[$extra] = $what;
 		}
-
-		# in case someone decides to be cute and start doing an
-		# auto-incrementing attack on user-supplied parameters...
-		# (20110302/straup)
-
-		if ((is_array($details)) && (! in_array($what, $details))){
-			continue;
-		}
-
-		$export_more[$extra] = $what;
 	}
 
 	# caching?
@@ -107,7 +104,7 @@
 
 		$ok_cache = 1;
 
-		if (! in_array($format, $GLOBALS['cfg']['export_cache_valid_formats'])){
+		if (in_array($format, $GLOBALS['cfg']['export_cache_exclude_formats'])){
 			$ok_cache = 0;
 		}
 
