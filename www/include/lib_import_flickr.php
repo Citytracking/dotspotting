52540ec6e239faa838797dee1e14b3043a0df0e1
straup <straup@gmail.com>
2011-04-25 22:43:32 +0000

    tighten up calls to flickr api

--- www/include/lib_import_flickr.php
+++ www/include/lib_import_flickr.php
@@ -2,7 +2,65 @@
 
 	loadlib("flickr");
 
-	$GLOBALS['import_flickr_spr_extras'] = 'geo';
+	# A comma-delimited list of extra information to fetch for each returned record.
+	# Currently supported fields are: description, license, date_upload, date_taken,
+	# owner_name, icon_server, original_format, last_update, geo, tags, machine_tags,
+	# o_dims, views, media, path_alias, url_sq, url_t, url_s, url_m, url_z, url_l,
+	# url_o (http://www.flickr.com/services/api/flickr.photos.search.htm)
+
+	$GLOBALS['import_flickr_spr_extras'] = 'geo,description,date_taken,owner_name,tags';
+
+	#################################################################
+
+	function import_flickr_url($url, $more=array()){
+
+		$empty = array();
+
+		# photosets
+
+		if (preg_match("!/sets/(\d+)/?$!", $url, $m)){
+			return import_flickr_photoset($m[1], $more);
+		}
+
+		# groups
+
+		if (preg_match("!/groups/([^/]+)(?:/pool)?/?$!", $url, $m)){
+
+			$group_id = $m[1];
+
+			if (! preg_match("!\@N\d+$!", $group_id)){
+				$group_id = flickr_lookup_group_id_by_url($url);
+			}
+
+			if (! $group_id){
+				return $empty;
+			}
+
+			return import_flickr_group_pool($group_id, $more);
+		}
+
+		# individual users
+
+		if (preg_match("!/photos/([^/]+)/?!", $url, $m)){
+
+			$user_id = $m[1];
+
+			if (! preg_match("!\@N\d+$!", $user_id)){
+				$user_id = flickr_lookup_user_id_by_url($url);
+			}
+
+			if (! $user_id){
+				return $empty;
+			}
+
+			return import_flickr_user($user_id, $more);
+
+		}
+
+		# yahoo says no
+
+		return $empty;
+	}
 
 	#################################################################
 
@@ -34,7 +92,6 @@
 
 		$more = array(
 			'root' => 'photoset',
-			'ensure_geo' => 1,
 		);
 
 		return import_flickr_spr_paginate($method, $args, $more);
@@ -71,13 +128,29 @@
 		$root = $more['root'];
 
 		$photos = array();
+		$count_photos = 0;
+
+		$to_remove = array(
+			'secret',
+			'server',
+			'farm',
+			'isprimary',
+			'place_id',
+			'geo_is_family',
+			'geo_is_friend',
+			'geo_is_contact',
+			'geo_is_public',
+			'datetakengranularity',
+		);
 
 		$page = 1;
 		$pages = null;
 
 		while ((! isset($pages)) || ($page <= $pages)){
 
+			$args['per_page'] = 500;
 			$args['page'] = $page;
+
 			$_rsp = flickr_api_call($method, $args);
 
 			if ($_rsp['ok']){
@@ -88,27 +161,33 @@
 					$pages = $rsp[$root]['pages'];
 				}
 
-				if ($more['ensure_geo']){
+				foreach ($rsp[$root]['photo'] as $ph){
 
-					foreach ($rsp[$root]['photo'] as $ph){
+					# why didn't we just add a "has_geo" attribute
+					# to the API responses... (20110425/straup)
 
-						# why didn't we just add a "has_geo" property
-						# to the API... (20110425/straup)
+					if ($ph['accuracy'] == 0){
+						continue;
+					}
 
-						if ($ph['accuracy'] != 0){
-							$photos[] = $ph;
+					foreach ($to_remove as $key){
+						if (isset($ph[$key])){
+							unset($ph[$key]);
 						}
 					}
-				}
 
-				else {
-					$photos = array_merge($photos, $rsp[$root]['photo']);
-				}
+					$ph['description'] = $ph['description']['_content'];
+					$photos[] = $ph;
+					$count_photos += 1;
 
-				if ((isset($more['max_photos'])) && (count($photos) >= $more['max_photos'])){
-					$photos = array_slice($photos, 0, $more['max_photos']);
-					break;
+					if ((isset($more['max_photos'])) && ($count_photos >= $more['max_photos'])){
+						break;
+					}
 				}
+			}
+
+			if ((isset($more['max_photos'])) && ($count_photos >= $more['max_photos'])){
+				break;
 			}
 
 			$page += 1;
