62ce8ec5728fd472ef72ed4fff681dbb536b9b19
straup <straup@gmail.com>
2011-06-10 23:38:23 +0000

    flesh it out a bit

--- www/include/lib_dots_address.php
+++ www/include/lib_dots_address.php
@@ -11,24 +11,65 @@
 		# try to find corresponding matches for in $dot
 
 		$addr = array(
-			'street' => '',
-			'city' => '',
-			'state' => '',
-			'country' => '',
-			'zip' => '',
+			'street' => array(),
+			'city' => array(),
+			'state' => array(),
+			'country' => array(),
+			'zip' => array(),
 		);
 
-		# first just lower-case everything so as to
-		# minimize the desire to stab ourselves in the
-		# face.
-
-		$tmp = array();
-
 		foreach ($dot as $k => $v){
-			$tmp[ strtolower($k) ] = $v;
-		}
 
-		# do stuff here
+			if (trim($v) == ''){
+				continue;
+			}
+
+			# first, normalize everything so as to minimize
+			# the desire to stab ourselves in the face...
+
+			$k = str_replace(" ", "", $k);
+			$k = str_replace("_", "", $k);
+			$k = strtolower($k);
+
+			# make sure this is always placed in front of
+			# anything else that might be classified as a
+			# street (like 'address' below)
+
+			if (preg_match("/^street/", $k)){
+				array_unshift($addr['street'], $v);
+				continue;
+			}
+
+			if ($k == 'address'){
+				$addr['street'][] = $v;
+				continue;
+			}
+
+			if (preg_match("/^(?:city|town)/", $k)){
+				$addr['city'][] = $v;
+				continue;
+			}
+
+			if ($k == 'borough'){
+				$addr['city'][] = $v;
+				continue;
+			}
+
+			if ($k == 'state'){
+				$addr['state'][] = $v;
+				continue;
+			}
+
+			if ($k == 'country'){
+				$addr['country'][] = $v;
+				continue;
+			}
+
+			if (preg_match("/^(?:zip|postal)/", $k)){
+				$addr['zip'][] = $v;
+				continue;
+			}
+		}
 
 		# okay, now iterate over the stuff we've found
 		# and return a comma-separated string
@@ -37,11 +78,11 @@
 
 		foreach ($addr as $k => $v){
 
-			if (! $addr[$k]){
+			if (! count($addr[$k])){
 				continue;
 			}
 
-			$rsp[] = $v;
+			$rsp[] = implode(" ", $v);
 		}
 
 		return implode(", ", $rsp);
