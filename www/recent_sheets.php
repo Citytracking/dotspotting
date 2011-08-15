b80264632c8d115d19578c85702048bd25dc4ee0
sean connelley <connelley@stamen.com>
2011-08-15 19:16:05 +0000

    pagination added on "Recent Sheets" page added a global pagination style variable

--- www/recent_sheets.php
+++ www/recent_sheets.php
@@ -5,11 +5,19 @@
 	#
 
 	include("include/init.php");
-
-	$recent_sheets = sheets_recently_created($GLOBALS['cfg']['user_id']);
+	
+	// pagination...
+	$more = array(
+      "page" => get_int32("page")  
+    );
+    $pagination_url = "{$GLOBALS['cfg']['abs_root_url']}recent/sheets/";
+    $GLOBALS['smarty']->assign("pagination_url",$pagination_url);
+    
+    // go get sheets
+	$recent_sheets = sheets_recently_created($GLOBALS['cfg']['user_id'],$more);
 	$GLOBALS['smarty']->assign_by_ref("recent_sheets", $recent_sheets['sheets']);
 	
-
+    // display sheets
 	$smarty->display('page_recent_sheets.txt');
 	exit();
 
