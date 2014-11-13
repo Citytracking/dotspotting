169f61a7864a94b7515d09fa7de5571c75fb50d5
Sean Connelley <connelley@stamen.com>
2014-11-13 17:53:00 -0500

    Fixed deprecated /e modifier

--- www/include/lib_filter.php
+++ www/include/lib_filter.php
@@ -106,7 +106,7 @@
 
 
 		#
-		# these non-numeric entities are allowed. non allowed entities will be 
+		# these non-numeric entities are allowed. non allowed entities will be
 		# converted from '&foo;' to '&amp;foo;'
 		#
 
@@ -162,7 +162,10 @@
 
 		function escape_comments($data){
 
-			$data = preg_replace("/<!--(.*?)-->/se", "'<!--'.HtmlSpecialChars(\$this->StripSingle('\\1')).'-->'", $data);
+			$data = preg_replace_callback("/<!--(.*?)-->/s",
+				function($matches){
+					return "'<!--'.HtmlSpecialChars($this->StripSingle($matches[1])).'-->'";
+				}, $data);
 
 			return $data;
 		}
@@ -183,7 +186,6 @@
 				$data = preg_replace("/^>/", "", $data);
 				$data = preg_replace("/<([^>]*?)(?=<|$)/", "<$1>", $data);
 				$data = preg_replace("/(^|>)([^<]*?)(?=>)/", "$1<$2", $data);
-
 			}else{
 
 				#
@@ -192,7 +194,6 @@
 
 				$data = preg_replace("/<([^>]*?)(?=<|$)/", "&lt;$1", $data);
 				$data = preg_replace("/(^|>)([^<]*?)(?=>)/", "$1$2&gt;<", $data);
-
 				#
 				# the last regexp causes '<>' entities to appear
 				# (we need to do a lookahead assertion so that the last bracket can
@@ -212,7 +213,9 @@
 
 		function check_tags($data){
 
-			$data = preg_replace("/<(.*?)>/se", "\$this->process_tag(\$this->StripSingle('\\1'))",	$data);
+			$data = preg_replace_callback("/<(.*?)>/s", function($m){
+				return $this->process_tag($this->StripSingle($m[1]));
+				},	$data);
 
 			foreach(array_keys($this->tag_counts) as $tag){
 				for($i=0; $i<$this->tag_counts[$tag]; $i++){
@@ -340,7 +343,7 @@
 		#####################################################################################
 
 		#
-		# given some HTML input, find out if the non-HTML part is too 
+		# given some HTML input, find out if the non-HTML part is too
 		# shouty. that is, does it solely consist of capital letters.
 		# if so, make it less shouty.
 		#
@@ -388,7 +391,7 @@
 
 		#
 		# given a block of non-HTML, filter it for shoutyness by lowercasing
-		# the whole thing and then capitalizing the first letter of each 
+		# the whole thing and then capitalizing the first letter of each
 		# 'sentance'.
 		#
 
@@ -414,19 +417,19 @@
 		#
 		# this function is called in two places - inside of each href-like
 		# attributes and then on the whole document. it's job is to make
-		# sure that anything that looks like an entity (starts with an 
+		# sure that anything that looks like an entity (starts with an
 		# ampersand) is allowed, else corrects it.
 		#
 
 		function validate_entities($data, $in_attribute){
 
-			#		
+			#
 			# turn ascii characters into their actual characters, if requested.
 			# we need to always do this inside URLs to avoid people using
 			# entities or URL escapes to insert 'javascript:' or something like
 			# that. outside of attributes, we optionally filter entities to
 			# stop people from inserting text that they shouldn't (since it might
-			# make it into a clickable URL via lib_autolink). 
+			# make it into a clickable URL via lib_autolink).
 			#
 
 			if ($in_attribute || $this->normalise_ascii_entities){
@@ -440,9 +443,11 @@
 			# it).
 			#
 
-			$data = preg_replace(
-				'!&([^&;]*)(?=(;|&|$))!e',
-				"\$this->check_entity(\$this->StripSingle('\\1'), \$this->StripSingle('\\2'))",
+			$data = preg_replace_callback(
+				'!&([^&;]*)(?=(;|&|$))!',
+				function ($matches){
+					return $this->check_entity($this->StripSingle($matches[1]), $this->StripSingle($matches[2]));
+				},
 				$data
 			);
 
@@ -463,7 +468,7 @@
 				array($this, 'cleanup_non_tags_inner'),
 				$data
 			);
-			
+
 		}
 
 		function cleanup_non_tags_inner($m){
@@ -476,7 +481,7 @@
 
 
 			#
-			# find any literal quotes outside of tags and replace them 
+			# find any literal quotes outside of tags and replace them
 			# with &quot;. we call it last thing before returning.
 			#
 
@@ -491,9 +496,9 @@
 		#####################################################################################
 
 		#
-		# this function gets passed the 'inside' and 'end' of a suspected 
-		# entity. the ampersand is not included, but must be part of the 
-		# return value. $term is a look-ahead assertion, so don't return 
+		# this function gets passed the 'inside' and 'end' of a suspected
+		# entity. the ampersand is not included, but must be part of the
+		# return value. $term is a look-ahead assertion, so don't return
 		# it.
 		#
 
@@ -601,7 +606,7 @@
 
 		#
 		# given a character code and the starting escape character (either '%' or '&'),
-		# return either a hex entity (if the character code is non-ascii), or a raw 
+		# return either a hex entity (if the character code is non-ascii), or a raw
 		# character. remeber to escape XML characters!
 		#
 
