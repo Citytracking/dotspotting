7dc2e2dfa53d01629e9fe1d9b13045cc425531bc
Sean Connelley <connelley@stamen.com>
2014-11-13 19:50:36 -0500

    Filter lib scope

--- www/include/lib_filter.php
+++ www/include/lib_filter.php
@@ -212,9 +212,9 @@
 		#####################################################################################
 
 		function check_tags($data){
-
+			$tt = $this;
 			$data = preg_replace_callback("/<(.*?)>/s", function($m){
-				return process_tag(StripSingle($m[1]));
+				return $tt->process_tag($tt->StripSingle($m[1]));
 				},	$data);
 
 			foreach(array_keys($this->tag_counts) as $tag){
